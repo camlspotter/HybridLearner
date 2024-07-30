38e55bc3626ffb711b51a499dc2db070d8987103
Jun Furuse <jun.furuse@dailambda.jp>
2024-07-30 10:40:20 +0900

    run_main_HybridLearner.sh: automate MATLAB engine sharing

--- build/run_main_HybridLearner.sh
+++ build/run_main_HybridLearner.sh
@@ -1,16 +1,42 @@
 #!/bin/bash
+
+set -e
+
 # This file must be executed from the folder HybridLearner/build but before executing please perform the TODO below.
 
-# TODO:
-#  (1) Open a Terminal and start a MATLAB instance using the command:
-#  matlab &
-# (2) Then in the Command Window of MATLAB just started, type the command
-#  matlab.engine.shareEngine
-# This will save the time for starting MATLAB and Simulink for every experiment.
+source ../config
+echo MATLAB_ROOT=$MATLAB_ROOT
 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib:/usr/local/lib:/usr/lib/x86_64-linux-gnu:$MATLAB_ROOT/bin/glnxa64:$MATLAB_ROOT/bin/glnxa64/builtins/sl_main/:$MATLAB_ROOT/extern/bin/glnxa64:$MATLAB_ROOT/extern/lib/glnxa64:$MATLAB_ROOT/sys/os/glnxa64
+export PATH=$PATH:/usr/lib/x86_64-linux-gnu/glib-2.0/:$MATLAB_ROOT/bin
+export DISPLAY=
+export LANG=en
+
+FIFODIR=$(mktemp -d)
+FIFO=$FIFODIR/fifo
+mkfifo $FIFO
+
+echo Launching MATLAB...
+tail -f $FIFO | matlab -nodisplay -r "matlab.engine.shareEngine" &
+PID=$!
+echo Launched MATLAB PID=$PID
+
+close_matlab ()
+{
+    if [ -e $FIFO ]; then
+	echo Closing MATLAB...
+	echo quit > $FIFO
+	echo Closed MATLAB
+	rm -rf $FIFODIR
+    fi
+}
+
+trap close_matlab ERR INT
 
 # Execute the shell script for learning the benchmark models
-./run_results_main_learn_models.sh  # This took approximately 2 hours on Intel i9 CPU 2.4 GHz with 32 GB RAM
+# This took approximately 2 hours on Intel i9 CPU 2.4 GHz with 32 GB RAM
+./run_results_main_learn_models.sh
+
 echo "Done Learning Models"
 
 # Before executing this, we assume the script run_results_main_learn_models is already executed and models are learned and copied in the respective folders.
@@ -21,3 +47,5 @@ echo "Done generating output Trajectories"
 # have already been excuted and models are learned and output trajectories are generated and copied in the respective folders.
 python3 ../examples/computeDTW_dist_main.py
 echo "Done creating summary_results.csv; DTW distances between learned and Test Trajectories"
+
+close_matlab
